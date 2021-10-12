66902c98caacdef1a4fc49a06a418c10a3ad3e09
Gürkan Hüray <huraygu@CZOST-N-238.group.corp.storaenso.com>
2021-10-12 23:09:55 +0300

    Updates

---
+++ precache-manifest.3fb7d994b97cff48d01b4d5f0a24a8ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6408eb0e716b2074ea8ebf3ee1ad9c",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "863c5422c04c67b527bd",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863c5422c04c67b527bd",
+    "url": "/home/static/js/main.0f419293.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "00aa9bc57e7187043d07d472f814ad1e",
+    "url": "/home/static/media/resume.00aa9bc5.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.3fb7d994b97cff48d01b4d5f0a24a8ea.js"
 );
 
 self.addEventListener('message', (event) => {
